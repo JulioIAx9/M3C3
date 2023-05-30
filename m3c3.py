4c0a576bbd5541546e54c7c62ab8caeb1ce5247e
JulioIAx9 <123417516+JulioIAx9@users.noreply.github.com>
2023-05-30 18:10:58 -0400

    Update m3c3.py
    propuesta con las definiuciones requeridas

--- m3c3.py
+++ m3c3.py
@@ -1,3 +1,18 @@
+def hacer_grandioso(lista_magos):
+    """
+    Modifica la lista de magos añadiendo 'El gran' antes del nombre de cada mago.
+    """
+    for i in range(len(lista_magos)):
+        lista_magos[i] = 'El gran ' + lista_magos[i]
+
+def imprimir_nombres(lista):
+    """
+    Imprime el nombre de cada persona en la lista.
+    """
+    for nombre in lista:
+        print(nombre)
+
+
 nombres = [
     "Harry Houdini",
     "Newton",
@@ -11,9 +26,9 @@ nombres = [
     "Juanes"
 ]
 # Imprimir la lista original sin modificaciones
-print("Lista completa de nombres:")
-for nombre in nombres:
-    print(nombre)
+# print("Lista completa de nombres:")
+# for nombre in nombres:
+#    print(nombre)
 
 # Vamos a crear las listas de magos y científicos
 magos = ["Harry Houdini", "David Blaine", "Teller", "Julito el Hermoso"] # Lo de julito fue para probar el string y me gusto XD, se puede omitir
@@ -24,24 +39,24 @@ cientificos = ["Newton", "Hawking", "Einstein"]
 otros = list(set(nombres) - set(magos) - set(cientificos)) # se entiende mejor y me ahorro codigo!!
 
 # Modificamos directamente la lista de magos añadiendo la frase "El gran"
-for i in range(len(nombres)):
-    if nombres[i] in magos:
-        nombres[i] = "El gran " + nombres[i]
-
+hacer_grandioso(magos)
+# for i in range(len(nombres)):
+#    if nombres[i] in magos:
+#        nombres[i] = "El gran " + nombres[i]
+#     return "El gran " + nombre
 # Continuar con Imprimir cada una de las listas requeridas faltantes
 
-# print("Lista completa de nombres:") ( preferí subirla e imprimirla antes de la modificacion de la lista, economía es riqueza)
+print("Lista completa de nombres:")
 # for nombre in nombres:
 #    print(nombre)
-
+imprimir_nombres(nombres)
 print("\nNombres de los magos grandiosos:")
-for mago in magos:
-    print("El gran " + mago)
-
+# for mago in magos:
+#    print("El gran " + mago)
+imprimir_nombres(magos)
 print("\nNombres de los científicos:")
-for cientifico in cientificos:
-    print(cientifico)
-
+# for cientifico in cientifico
+imprimir_nombres(cientificos)
 print("\nLista de otros nombres:")
-for otro in otros:
-    print(otro)
+#  for otro in otros
+imprimir_nombres(otros)
